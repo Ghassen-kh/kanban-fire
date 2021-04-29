a12545dacd6a87996e4a84ca578d3755af3006a5
ghassen <ghassen.khalouaoui15@gmail.com>
2021-04-29 06:45:36 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBmLBIeQhevgS8wT0H_s8PNf_jiMzUUjsc",
+    authDomain: "kanban-fire-ghassen.firebaseapp.com",
+    projectId: "kanban-fire-ghassen",
+    storageBucket: "kanban-fire-ghassen.appspot.com",
+    messagingSenderId: "42719594163",
+    appId: "1:42719594163:web:39c95b20ae4897dfd092c6",
+    measurementId: "G-G1N1JCCFG0"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBmLBIeQhevgS8wT0H_s8PNf_jiMzUUjsc",
+    authDomain: "kanban-fire-ghassen.firebaseapp.com",
+    projectId: "kanban-fire-ghassen",
+    storageBucket: "kanban-fire-ghassen.appspot.com",
+    messagingSenderId: "42719594163",
+    appId: "1:42719594163:web:39c95b20ae4897dfd092c6",
+    measurementId: "G-G1N1JCCFG0"
+  }
 };
 
 /*
